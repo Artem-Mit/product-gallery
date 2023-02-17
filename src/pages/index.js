e2b336adb17950cbdaa723972dcb9ec9bab1e84f
Artem <artem.v.mitin@yandex.ru>
2023-02-17 17:40:51 +0300

    bugfix: fix .ready jquery

--- src/pages/index.js
+++ src/pages/index.js
@@ -65,7 +65,7 @@ const swiper = new Swiper(".swiper", {
   }
 });
 
-$(document).ready(function () {
+$(function () {
   $(".owl-carousel").owlCarousel({
     dotsClass: 'owl__dots-container',
     responsive: {
